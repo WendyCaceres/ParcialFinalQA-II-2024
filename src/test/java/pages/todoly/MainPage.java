9c698757f7a97cf8489a99a29b29c3d0f6057ef8
WendyCaceres <wcaceres683@gmail.com>
2024-11-05 17:29:00 -0400

    Requisitos examen

---
+++ src/test/java/pages/todoly/MainPage.java
@@ -0,0 +1,9 @@
+package pages.todoly;
+
+import controls.Button;
+import org.openqa.selenium.By;
+
+public class MainPage {
+   public Button loginButton = new Button(By.xpath("//img[@src=\"/Images/design/pagelogin.png\"]"));
+   public Button signUpButton = new Button(By.xpath("//img[@src='/Images/design/pagesignup.png']"));
+}
