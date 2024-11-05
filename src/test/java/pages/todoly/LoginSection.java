74ee96c01646e79ba60d4306f27a42b21c2b87a4
WendyCaceres <wcaceres683@gmail.com>
2024-11-05 17:56:29 -0400

    Segundo Commit

--- src/test/java/pages/todoly/LoginSection.java
+++ src/test/java/pages/todoly/LoginSection.java
@@ -5,13 +5,30 @@ import controls.TextBox;
 import org.openqa.selenium.By;
 
 public class LoginSection {
+    public Button loginButton = new Button(By.xpath("//img[@src='/Images/design/pagelogin.png']"));
+
     public TextBox emailTxtBox = new TextBox(By.id("ctl00_MainContent_LoginControl1_TextBoxEmail"));
     public TextBox pwdTxtBox = new TextBox(By.id("ctl00_MainContent_LoginControl1_TextBoxPassword"));
-    public  Button loginButton = new Button(By.id("ctl00_MainContent_LoginControl1_ButtonLogin"));
+    public  Button entryButton = new Button(By.id("ctl00_MainContent_LoginControl1_ButtonLogin"));
 
+    public Button createAccountButton = new Button(By.xpath("//img[@src='/Images/design/pagesignup.png']"));
+    public TextBox fullName = new TextBox(By.id("ctl00_MainContent_SignupControl1_TextBoxFullName"));
+    public TextBox email = new TextBox(By.id("ctl00_MainContent_SignupControl1_TextBoxEmail"));
+    public TextBox pwd = new TextBox(By.id("ctl00_MainContent_SignupControl1_TextBoxPassword"));
+    public Button agreeButton = new Button(By.id("ctl00$MainContent$SignupControl1$CheckBoxTerms"));
+    public Button singupButton = new Button(By.id("ctl00_MainContent_SignupControl1_ButtonSignup"));
     public void login(String email, String pwd){
         this.emailTxtBox.setText(email);
         this.pwdTxtBox.setText(pwd);
-        this.loginButton.click();
+        this.entryButton.click();
+    }
+
+    public void createAccount(String fullName, String email,String pwd){
+        this.fullName.setText(fullName);
+        this.email.setText(fullName);
+        this.pwd.click();
+        agreeButton.click();
+        singupButton.click();
+
     }
 }
--- src/test/java/pages/todoly/SettingsSection.java
+++ src/test/java/pages/todoly/SettingsSection.java
@@ -1,22 +1,23 @@
 package pages.todoly;
 
+
 import controls.Button;
 import controls.Label;
 import controls.TextBox;
 import org.openqa.selenium.By;
 
+
 public class SettingsSection {
-    public TextBox fullNameTextbox = new TextBox(By.xpath("//input[@id='FullNameInput']"));
+    public Button okButton = new Button(By.xpath("//button/span[text()='Ok']"));
+    public TextBox emailInput = new TextBox(By.id("EmailInput"));
 
-    public Button okButton = new Button(By.xpath("//button[span[text()='Ok']]"));
-    public Label accountTab = new Label(By.xpath("//a[@href='#settings_Account']"));
-    public Button deleteAccountButton = new Button(By.id("DeleteAccountBtn"));
+    public Label getEmail(String email){
+        return new Label(By.id(" //input[contains(@value,'"+email+"')]"));
 
-    public void navigateToAccountTab() {
-        accountTab.click();
     }
 
-    public void deleteAccount() {
-        deleteAccountButton.click();
+    public void editEmail(String email){
+        emailInput.clearSetText(email);
+        okButton.click();
     }
 }
